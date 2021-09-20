5d48b2ccdef3c3c5e1e1854f883afdc1348cc923
yakoda-sudo <85778625+yakoda-sudo@users.noreply.github.com>
2021-09-20 13:16:35 +0800

    Update run_camera_upload.sh

--- run_camera_upload.sh
+++ run_camera_upload.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd /home/stevek
 filename=image-"`date +"%Y-%m-%d-%H%M"`".jpg
-url="https://hackerphotoea.blob.core.windows.net/team/${filename}?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacuptfx&se=2022-09-16T21:11:26Z&st=2021-09-16T13:11:26Z&spr=https&sig=CQY9NQTfQWg4XvId%2FHr3E%2FcJxNbCQpZzU8XHLrN0m8w%3D"
+url="https://hackerphotoea.blob.core.windows.net/team/${filename}?sv=[your_storage_account_sas_token_here]"
 sudo /usr/bin/raspistill -o /home/stevek/$filename -w 1200 -h 1600 -t 1000 -a "Hello from your team"
 curl -X PUT -T ./image-"`date +"%Y-%m-%d-%H%M"`".jpg -H "x-ms-date: $(TZ=GMT date '+%a, %d %h %Y %H:%M:%S %Z')" -H "x-ms-blob-type: BlockBlob" "$url"
 exit
